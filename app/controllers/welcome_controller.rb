b6a7efe1d54f6941daa7dbcc2e16a5fe58389da8
tomoya <ihatov08@gmail.com>
2016-07-05 16:04:38 +0900

    ransackで検索機能の実装

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,14 @@
 class WelcomeController < ApplicationController
   def index
-    @events = Event.page(params[:page]).per(1).where('start_time > ?', Time.zone.now).order(:start_time)
+    @q = Event.page(params[:page]).per(10).order(:start_time).search(search_params)
+    @events = @q.result(distinct: true)
+  end
+
+  private
+
+  def search_params
+    params.require(:q).permit(:name_cont, :start_time_gteq)
+  rescue
+    { start_time_gteq: Time.zone.now }
   end
 end
